ac8e72afc2fea395515dee4c08d023f555addef4
K. Eaglin <kceaglin@gmail.com>
2018-05-22 04:43:07 -0400

    troubleshooting heroku deployment

--- lfg_api/settings.py
+++ lfg_api/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'lfg_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'lfg_db',
-#         'USER': 'kevon',
-#         'PASSWORD': 'lfgpass',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'dev': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lfg_db',
+        'USER': 'kevon',
+        'PASSWORD': 'lfgpass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
