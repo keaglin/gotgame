d8ef987d12cb93fbf4b95628b80cdcfeac4c817e
K. Eaglin <kceaglin@gmail.com>
2018-05-22 04:36:13 -0400

    troubleshooting heroku deployment

--- lfg_api/settings.py
+++ lfg_api/settings.py
@@ -129,3 +129,6 @@ MEDIA_URL = '/media/'
 
 import django_heroku
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
